69c1c45d1ae970180f410427b88a8ddb134ba440
Shailen Narainsamy (ZA-DUR) <shailen.narainsamy@derivco.co.za>
2022-10-12 14:56:03 +0200

    demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./butter.png";
-// import logo from "./logo.svg";
+// import logo from "./butter.png";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Butter is the way !</p>
-        {/* <p> This is my react project!</p> */}
+        {/* <p>Butter is the way !</p> */}
+        <p> This is my react project!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
